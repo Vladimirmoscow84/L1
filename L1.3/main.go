bb3b5f8e4bdf5866f9948f030d51d88fae383e80
Vladimirmoscow84 <ccr1@yandex.ru>
2025-07-08 01:11:25 +0300

    l1.3 begin

--- L1.3/main.go
+++ L1.3/main.go
@@ -1,11 +1,38 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+)
+
 /*Реализовать постоянную запись данных в канал (в главной горутине).
 
 Реализовать набор из N воркеров, которые читают данные из этого канала и выводят их в stdout.
 
 Программа должна принимать параметром количество воркеров и при старте создавать указанное число горутин-воркеров.*/
 
+func readFromWorkers(cOut chan int, wg *sync.WaitGroup) int {
+	value := <-cOut
+	wg.Done()
+	return value
+}
+
 func main() {
+	var numberWorkers int
+	fmt.Println("Введите количество воркеров")
+	fmt.Scan(&numberWorkers)
+	cIn := make(chan int)
+	for i := 0; i < 5; i++ {
+		cIn <- i
+	}
+
+	wg := sync.WaitGroup{}
+	for range numberWorkers {
+		wg.Add(1)
+		go readFromWorkers(cIn, &wg)
+	}
+
+	wg.Wait()
+	close(cIn)
 
 }
