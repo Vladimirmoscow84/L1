c39c82158ebb967bc8706feea534196884a6af22
Vladimirmoscow84 <ccr1@yandex.ru>
2025-08-04 13:06:11 +0300

    L1.20 fix

--- L1.20/main.go
+++ L1.20/main.go
@@ -17,11 +17,16 @@ func main() {
 	data := "sun dog snow"
 	fmt.Printf("исходная строка: %s\n", data)
 
-	slice := strings.Split(data, " ")
-	for i, j := 0, len(slice)-1; i < j; i, j = i+1, j-1 {
-		slice[i], slice[j] = slice[j], slice[i]
-	}
+	fmt.Printf("строка после реверса: %s\n", reverse(data))
 
-	fmt.Printf("строка после реверса: %s\n", strings.Join(slice, " "))
+}
+func reverse(s string) string {
+	words := strings.Fields(s)
+
+	for i, j := 0, len(words)-1; i < j; i, j = i+1, j-1 {
+		words[i], words[j] = words[j], words[i]
+	}
+	s = strings.Join(words, " ")
+	return s
 
 }
